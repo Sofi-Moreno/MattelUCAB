579c839fa1adad2a198eb6f7493bdcab858da6e3
Sofia Moreno <sgmoreno.23@est.ucab.edu.ve>
2026-06-15 00:46:52 -0400

    Se organizo el directorio del proyecto y se creo el header y footer de la pagina

--- mattel-app/src/app/app.routes.ts
+++ mattel-app/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Designs } from './view/product_designs/designs';
+import { Home } from './view/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component:  Home},
+    {path: 'designs', component:  Designs}
+];
