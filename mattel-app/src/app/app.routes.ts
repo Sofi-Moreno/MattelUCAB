95e49aa86b638baddeaac61f768e6ad5ab288654
Andres Martinez <andmart1109@gmail.com>
2026-06-22 10:48:27 -0400

    vistas gestion roles y privilegios

--- mattel-app/src/app/app.routes.ts
+++ mattel-app/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { Home } from './view/home/home';
 import { Login } from './view/login/login';
 import { User } from './view/user/user';
 import { Profile } from './view/profile/profile';
+import { Roles } from './view/roles/roles';
 
 export const routes: Routes = [
     { path: '', component: Login },
     { path: 'login', component: Login },
     { path: 'home', component: Home },
     { path: 'user', component: User },
+    { path: 'roles', component: Roles },
     { path: 'designs', component: Designs },
     { path: 'profile', component: Profile },
 ];
